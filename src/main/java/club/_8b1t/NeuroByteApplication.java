9b94228193f4490b45468abd178ca697dbbe9137
wangzhijie0602 <46142484+wangzhijie0602@users.noreply.github.com>
2025-06-24 18:23:36 +0800

    初始提交

---
+++ src/main/java/club/_8b1t/NeuroByteApplication.java
@@ -0,0 +1,13 @@
+package club._8b1t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroByteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuroByteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/_8b1t/NeuroByteApplicationTests.java
@@ -0,0 +1,13 @@
+package club._8b1t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroByteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
